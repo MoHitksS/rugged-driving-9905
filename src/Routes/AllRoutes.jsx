d1cf2690807adb69e0e5d8be78e99d501ac888a2
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-08-24 13:38:28 +0530

    Navbar Added

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <div>
+        <Routes>
+            {/* <Route path='/' element={}></Route> */}
+        </Routes>
+    </div>
   )
 }
 
