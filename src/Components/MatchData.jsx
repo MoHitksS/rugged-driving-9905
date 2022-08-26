acf6744ba1af14c342f284f0e312653c567f1717
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-08-26 17:33:47 +0530

    upadated

--- src/Components/MatchData.jsx
+++ src/Components/MatchData.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import styles from '../CSS/MatchData.module.css'
-import { Stack } from '@chakra-ui/react'
+import { SimpleGrid, Stack } from '@chakra-ui/react'
 
 const MatchData = ({ matchType }) => {
     const [data, setData] = useState([])
@@ -16,32 +16,34 @@ const MatchData = ({ matchType }) => {
     }, [api]);
 
     return (
-        <div className={styles.matchDataContainer}>
+        // <div className={styles.matchDataContainer}>
+        <SimpleGrid columns={[1,1,4]} w={"100%"} gap={5}>
             {data?.map((ele, index) => (
-                <div className={styles.matchData} key={index}>
-                    <span><span className={styles.matchDataDay}>{ele.day},{ele.time}</span> • {ele.tournamnetType} • {ele.city}</span>
-                    <div className={styles.flagSection}>
-                        <img src={ele.flag1} alt={ele.team1} />
-                        <span>{ele.team1}</span>
+                    <div className={styles.matchData} key={index}>
+                        <span><span className={styles.matchDataDay}>{ele.day},{ele.time}</span> • {ele.tournamnetType} • {ele.city}</span>
+                        <div className={styles.flagSection}>
+                            <img src={ele.flag1} alt={ele.team1} />
+                            <span>{ele.team1}</span>
+                        </div>
+                        <div className={styles.flagSection}>
+                            <img src={ele.flag2} alt={ele.team2} />
+                            <span>{ele.team2}</span>
+                        </div>
+                        <Stack>
+                            {ele.matchTime !== "" ? <span className={styles.matchStart}>Match Starts in {ele.matchTime}</span> : <span className={styles.matchStart}>Match yet to begin</span>}
+                        </Stack>
+                        <Stack>
+                            <hr />
+                        </Stack>
+                        <div className={styles.bottomSection}>
+                            <span>Schedule</span>
+                            <span>Table</span>
+                            <span>Fantasy</span>
+                        </div>
                     </div>
-                    <div className={styles.flagSection}>
-                        <img src={ele.flag2} alt={ele.team2} />
-                        <span>{ele.team2}</span>
-                    </div>
-                    <Stack>
-                        {ele.matchTime !== "" ? <span className={styles.matchStart}>Match Starts in {ele.matchTime}</span> : <span className={styles.matchStart}>Match yet to begin</span>}
-                    </Stack>
-                    <Stack>
-                        <hr />
-                    </Stack>
-                    <div className={styles.bottomSection}>
-                        <span>Schedule</span>
-                        <span>Table</span>
-                        <span>Fantasy</span>
-                    </div>
-                </div>
             ))}
-        </div>
+        </SimpleGrid >
+        // </div>
     )
 }
 
