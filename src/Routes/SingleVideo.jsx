6053d6d8eb56463fc04ab92e5069f230280019ce
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-08-27 13:56:02 +0530

    Updated

--- src/Routes/SingleVideo.jsx
+++ src/Routes/SingleVideo.jsx
@@ -1,8 +1,22 @@
+import { Box, Flex } from '@chakra-ui/react'
 import React from 'react'
+import SidebarRightData from '../Components/SidebarRightData'
+import SingleVideoData from '../Components/SingleVideoData'
 
 const SingleVideo = () => {
   return (
-    <div>SingleVideo</div>
+    <Box>
+      <Box w={'100%'}>
+        <Flex w={'80%'} m={'auto'} gap={8}>
+          <Box w={"22%"}>
+            <SidebarRightData />
+          </Box>
+          <Box w={"75%"}>
+            <SingleVideoData />
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
   )
 }
 
